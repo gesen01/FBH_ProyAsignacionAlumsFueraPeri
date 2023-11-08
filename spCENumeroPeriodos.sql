ea6d356d7c1acccc04c7b10f6095c81a0e076787
gesen01 <gesen01@hotmail.com>
2023-11-08 17:16:18 -0600

    Se modifica la asignacion de periodos a los movimientos de ajuste de calificacion y calificacion extenporanea

--- spCENumeroPeriodos.sql
+++ spCENumeroPeriodos.sql
@@ -18,8 +18,18 @@ CREATE PROCEDURE spCENumeroPeriodos --SP para msostrar solo los Periodos de la a
 		           
 --//WITH ENCRYPTION
 AS BEGIN  
-  DECLARE @DurPeriodos int, @PeriodoInicio int, @Contador int  
+  DECLARE @DurPeriodos int
+		, @PeriodoInicio int
+		, @Contador int  
+		, @MovTipo VARCHAR(25)
   DECLARE @Tabla AS Table (Periodo int) 
+
+  SELECT @MovTipo=a.Mov
+  FROM CE a 
+  WHERE Empresa = @Empresa 
+  AND Sucursal = @Sucursal 
+  AND CicloEscolar = @CicloEscolar 
+  AND Materia = @Materia AND a.Grupo = @Grupo
 
   SELECT @DurPeriodos = ISNULL(DurPeriodos,0) FROM CEAsignacionProgramaPeriodo WHERE Empresa = @Empresa AND Sucursal = @Sucursal AND CicloEscolar = @CicloEscolar AND Materia = @Materia AND Grupo = @Grupo
   
@@ -34,7 +44,8 @@ AS BEGIN
     SET @Contador = @Contador + 1
   END 
   
-  --DELETE FROM @Tabla WHERE Periodo IN (SELECT ISNULL(PeriodoAsig,0) FROM CE a JOIN MovTipo b ON a.Mov = b.Mov AND Modulo = 'CE' AND Clave IN ('CE.CP','CE.ACL') WHERE Empresa = @Empresa AND Sucursal = @Sucursal AND CicloEscolar = @CicloEscolar AND Materia = @Materia AND a.Grupo = @Grupo AND Estatus = 'CONCLUIDO')
+ IF @MovTipo NOT IN ('Ajuste Calificacion','Calificacion exten')
+  DELETE FROM @Tabla WHERE Periodo IN (SELECT ISNULL(PeriodoAsig,0) FROM CE a JOIN MovTipo b ON a.Mov = b.Mov AND Modulo = 'CE' AND Clave IN ('CE.CP') WHERE Empresa = @Empresa AND Sucursal = @Sucursal AND CicloEscolar = @CicloEscolar AND Materia = @Materia AND a.Grupo = @Grupo AND Estatus = 'CONCLUIDO')
    
   SELECT * FROM @Tabla
 END
